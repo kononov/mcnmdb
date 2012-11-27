df9df89d4fd9211a51a65fbc6ce058c8483dc9cb
Klimin Mike <klinkin@gmail.com>
2012-11-27 18:21:29 +0400

    Added skeleton

--- auth.py
+++ auth.py
@@ -1,20 +1,33 @@
-class AuthUser(object):
-    pass
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .db import db
+from base import BaseMixin
 
-class SimpleAuthUser(AuthUser):
-    pass
+class AuthUser(db.Model, BaseMixin):
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))  # id пользователя
+    social_id = db.Column(db.Unicode(length=256), unique=True, nullable=True)
 
 class VkontakteAuthUser(AuthUser):
-    pass
+    __tablename__ = 'vk_auth_user'
 
 class FacebookAuthUser(AuthUser):
-    pass
+    __tablename__ = 'facebook_auth_user'
+
+    first_name = db.Column(db.String(255))
+    last_name  = db.Column(db.String(255))
+    email      = db.Column(db.String(255), nullable=False, unique=True)
+    token      = db.Column(db.UnicodeText(), nullable=False)
 
 class OdnoklassnikiAuthUser(AuthUser):
-    pass
+    __tablename__ = 'odnkl_auth_user'
 
 class MailRuAuthUser(AuthUser):
-    pass
+    __tablename__ = 'mailru_auth_user'
 
 class TwitterAuthUser(AuthUser):
-    pass
+    __tablename__ = 'twitter_auth_user'
+
+    screen_name = db.Column(db.String(255))
+    oauth_token = db.Column(db.UnicodeText(), nullable=False)
+    oauth_token_secret = db.Column(db.UnicodeText(), nullable=False)
