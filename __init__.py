20083f74c2dcca3820a245ba28fc4aacc30729db
Klimin Mike <klinkin@gmail.com>
2012-12-06 22:01:39 +0400

    Таблицы для сбора статистики по запросам и geo

--- __init__.py
+++ __init__.py
@@ -5,4 +5,5 @@ from shoplist import ShoppingListItem, ShoppingList
 from store import Corporation, Store, StoreGroup, StorePicture, StoreState
 from task import Task, TaskItem, TaskState, TaskItemState, TaskType
 from user import Role, Group, User, UserFavouriteStore
+from history import SearchHistory, GeoHistory
 from base import Alembic, save_model, save_models
---
+++ history.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from .db import db
+from base import BaseMixin, ByMixin
+
+TYPE_SOURCE_UNKNOWN = 0
+TYPE_SOURCE_WEB     = 1
+TYPE_SOURCE_IOS     = 2
+TYPE_SOURCE_ANDROID = 3
+
+
+class SearchHistory(db.Model, BaseMixin, ByMixin):
+    """
+    Таблица истории поиска пользователя
+    """
+
+    __tablename__ = 'search_history'
+
+    query   = db.Column(db.Unicode(100), nullable=False)  # строка поиска
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    type    = db.Column(db.Integer) # источник поиска
+
+
+class GeoHistory(db.Model, BaseMixin, ByMixin):
+    """
+    Таблица истории координат пользователей
+    """
+
+    __tablename__ = 'geo_history'
+
+    lat     = db.Column(db.Float()) # координата: широта
+    lng     = db.Column(db.Float()) # координата: долгота
+    type    = db.Column(db.Integer) # источник поиска
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
