efbf409be519b433927df79f79b412ed20162a99
Klimin Mike <klinkin@gmail.com>
2012-12-07 16:59:20 +0400

    + Добавил в __init__.py db + Добавил в __init__.py константы
    Теперь можно писать: from mcnm<prj>.models import все что нужно

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from db import db
 from account import Account, Transaction, TransactionType, AccountState, TransactionState
 from offer import Offer, Measure, OfferPicture, OfferState
 from settings import ShoppingListSettings, UserSettings, UserOption, ListOption
@@ -7,3 +8,22 @@ from task import Task, TaskItem, TaskState, TaskItemState, TaskType
 from user import Role, Group, User, UserFavouriteStore
 from history import SearchHistory, GeoHistory
 from base import Alembic, save_model, save_models
+
+from account import ( TRANSACTION_TYPE_UNKNOWN, TRANSACTION_TYPE_DEBIT, TRANSACTION_TYPE_CREDIT,
+                      ACCOUNT_STATE_UNKNOWN, ACCOUNT_STATE_ACTIVE, ACCOUNT_STATE_BLOCKED, ACCOUNT_STATE_ClOSED,
+                      TRANSACTION_STATE_UNKNOWN, TRANSACTION_STATE_OK, TRANSACTION_STATE_DELETE, TRANSACTION_STATE_STORN )
+
+from history import ( TYPE_SOURCE_UNKNOWN, TYPE_SOURCE_WEB, TYPE_SOURCE_IOS, TYPE_SOURCE_ANDROID )
+
+from offer import ( OFFER_STATE_UNKNOWN, OFFER_STATE_OK, OFFER_STATE_DELETE )
+
+from settings import ( USEROPTION_GPS, USEROPTION_PUBLISH_OFFER, USEROPTION_PUBLISH_PRICE, USEROPTION_PUBLISH_STORE,
+                       LISTOPTION_RADIUS_SEARCH, LISTOPTION_PRIORITET_SEARCH, LISTOPTION_SORT_BY )
+
+from store import ( STORE_STATE_UNKNOWN, STORE_STATE_OK, STORE_STATE_DELETE )
+
+from task import ( TASK_TYPE_UNKNOWN, TASK_TYPE_OFFER_LOAD, TASK_TYPE_STORE_LOAD,
+                   TASK_STATE_UNKNOWN, TASK_STATE_INIT, TASK_STATE_START, TASK_STATE_EXEC, TASK_STATE_FINISH_ERR, TASK_STATE_FINISH_SUCCES,
+                   TASK_ITEM_STATE_UNKNOWN, TASK_ITEM_STATE_OK, TASK_ITEM_STATE_ERROR )
+
+
