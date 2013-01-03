22193842968a541dba9d536d80960a76b7582c37
Klimin Mike <klinkin@gmail.com>
2013-01-03 04:29:31 +0400

    Debug

--- base.py
+++ base.py
@@ -210,7 +210,8 @@ class BaseMixin(IdMixin, UpdateMixin, TimesMixin):
             relations -= frozenset(exclude_columns)
         deep = dict((r, {}) for r in relations)
 
-        result = _to_dict(instance=self, deep=deep, exclude=exclude_columns, include=include_columns, exclude_relations=exclude_relations, include_relations=include_relations)
+#        result = _to_dict(instance=self, deep=deep, exclude=exclude_columns, include=include_columns, exclude_relations=exclude_relations, include_relations=include_relations)
+        result = _to_dict(instance=self, deep=deep, exclude=exclude_columns, exclude_relations=exclude_relations)
 
         return result
 
