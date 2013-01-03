598c7f406c05067881fd7e87e323d434236b6c93
Klimin Mike <klinkin@gmail.com>
2013-01-03 04:58:26 +0400

    Debug

--- base.py
+++ base.py
@@ -210,8 +210,9 @@ class BaseMixin(IdMixin, UpdateMixin, TimesMixin):
             relations -= frozenset(exclude_columns)
         deep = dict((r, {}) for r in relations)
 
+# FIXME: необходимо реализавать фильтрацию по атрибутам и связям 
 #        result = _to_dict(instance=self, deep=deep, exclude=exclude_columns, include=include_columns, exclude_relations=exclude_relations, include_relations=include_relations)
-        result = _to_dict(instance=self, deep=deep, exclude=exclude_columns, exclude_relations=exclude_relations)
+        result = _to_dict(instance=self, deep=deep)
 
         return result
 
